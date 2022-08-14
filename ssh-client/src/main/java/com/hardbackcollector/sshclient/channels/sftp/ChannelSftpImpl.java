edb093833acf804f3331eea14614a71958ce9ef7
tfonteyn <user@localhost>
2022-08-14 13:05:03 +0100

    fix import

--- ssh-client/src/main/java/com/hardbackcollector/sshclient/channels/sftp/ChannelSftpImpl.java
+++ ssh-client/src/main/java/com/hardbackcollector/sshclient/channels/sftp/ChannelSftpImpl.java
@@ -22,6 +22,7 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.PipedOutputStream;
 import java.io.UnsupportedEncodingException;
+import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.security.GeneralSecurityException;
 import java.util.ArrayList;
