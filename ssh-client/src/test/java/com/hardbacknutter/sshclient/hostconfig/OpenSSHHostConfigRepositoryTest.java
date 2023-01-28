0aa1173ad8dd2766185aa774dd81af9e4bc57e33
tfonteyn <user@localhost>
2023-01-28 19:12:26 +0000

    tests updated

--- ssh-client/src/test/java/com/hardbacknutter/sshclient/hostconfig/OpenSSHHostConfigRepositoryTest.java
+++ ssh-client/src/test/java/com/hardbacknutter/sshclient/hostconfig/OpenSSHHostConfigRepositoryTest.java
@@ -1,6 +1,7 @@
 package com.hardbacknutter.sshclient.hostconfig;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -15,6 +16,13 @@ import java.io.IOException;
 import java.security.GeneralSecurityException;
 import java.util.List;
 
+/**
+ * Test client:
+ * <pre>
+ * {@code ssh -Q cipher} to list all supported ciphers
+ * {@code ssh -G dev} to test/list the config for the host "dev"
+ * </pre>
+ */
 class OpenSSHHostConfigRepositoryTest
         extends BaseConnectionTest {
 
@@ -22,6 +30,7 @@ class OpenSSHHostConfigRepositoryTest
 
     private static final int ZIP = 0;
     private static final String COMPRESS_YES = "zlib@openssh.com,zlib,none";
+    private static final String COMPRESS_NO = "none,zlib@openssh.com,zlib";
 
     private HostConfigRepository repo;
 
@@ -36,102 +45,113 @@ class OpenSSHHostConfigRepositoryTest
     @Test
     void parseFile01() {
         final HostConfig host = repo.getHostConfig("");
-        System.out.println(host);
-
         assertNotNull(host);
-        assertEquals("root", host.getUser());
+        System.out.println(host);
+        assertEquals("ERROR", host.getString(HostConfig.LOG_LEVEL));
+
         assertTrue(host.getBooleanValue(HostConfig.COMPRESSION, false));
         assertEquals(COMPRESS_YES, host.getString(KexProposal.PROPOSAL_COMP_CTOS));
-        assertEquals("INFO", host.getString("LogLevel"));
+
+        assertEquals("root", host.getUser());
+
+        final List<String> ciphers = host.getStringList(HostConfig.CIPHERS, null);
+        System.out.println(ciphers);
+        assertEquals(2, ciphers.size());
+        assertEquals("aes256-ctr", ciphers.get(0));
+        assertEquals("rijndael-cbc@lysator.liu.se", ciphers.get(1));
     }
 
     @Test
     void parseFile02() {
         final HostConfig host = repo.getHostConfig("dev");
-        System.out.println(host);
-
         assertNotNull(host);
+        System.out.println(host);
+        assertEquals("ERROR", host.getString(HostConfig.LOG_LEVEL));
+
+        assertTrue(host.getBooleanValue(HostConfig.COMPRESSION, false));
+        assertEquals(COMPRESS_YES, host.getString(KexProposal.PROPOSAL_COMP_CTOS));
+
         assertEquals("john", host.getUser());
         assertEquals("dev.example.com", host.getHostname());
         assertEquals(2322, host.getPort());
+
+        final List<String> ciphers = host.getStringList(HostConfig.CIPHERS, null);
+        System.out.println(ciphers);
+        assertEquals(2, ciphers.size());
+        assertEquals("aes256-ctr", ciphers.get(0));
+        assertEquals("rijndael-cbc@lysator.liu.se", ciphers.get(1));
+
         assertEquals("~/.ssh/keys/dev_key", host.getString(HostConfig.IDENTITY_FILE));
-        assertTrue(host.getBooleanValue(HostConfig.COMPRESSION, false));
-        assertEquals(COMPRESS_YES, host.getString(KexProposal.PROPOSAL_COMP_CTOS));
-        assertEquals("INFO", host.getString("LogLevel"));
     }
 
     @Test
     void parseFile03() {
         final HostConfig host = repo.getHostConfig("targaryen");
-        System.out.println(host);
-
         assertNotNull(host);
+        System.out.println(host);
+        assertEquals("FATAL", host.getString(HostConfig.LOG_LEVEL));
+
+        assertTrue(host.getBooleanValue(HostConfig.COMPRESSION, false));
+        assertEquals(COMPRESS_YES, host.getString(KexProposal.PROPOSAL_COMP_CTOS));
+
         assertEquals("daenerys", host.getUser());
         assertEquals("192.168.1.10", host.getHostname());
         assertEquals(7654, host.getPort());
+
+        final List<String> ciphers = host.getStringList(HostConfig.CIPHERS, null);
+        System.out.println(ciphers);
+        assertEquals(3, ciphers.size());
+        assertEquals("aes256-ctr", ciphers.get(0));
+        assertEquals("rijndael-cbc@lysator.liu.se", ciphers.get(1));
+        assertEquals("aes192-cbc", ciphers.get(2));
+
         assertEquals("~/.ssh/targaryen.key", host.getString(HostConfig.IDENTITY_FILE));
-        assertTrue(host.getBooleanValue(HostConfig.COMPRESSION, false));
-        assertEquals(COMPRESS_YES, host.getString(KexProposal.PROPOSAL_COMP_CTOS));
-        assertEquals("INFO", host.getString("LogLevel"));
     }
 
     @Test
     void parseFile04() {
-        final HostConfig host = repo.getHostConfig("tyrell");
-        System.out.println(host);
-
+        final HostConfig host = repo.getHostConfig("martell");
         assertNotNull(host);
+        System.out.println(host);
+        assertNotEquals("INFO", host.getString(HostConfig.LOG_LEVEL));
+
+        assertFalse(host.getBooleanValue(HostConfig.COMPRESSION, false));
+        assertEquals(COMPRESS_NO, host.getString(KexProposal.PROPOSAL_COMP_CTOS));
+
         assertEquals("oberyn", host.getUser());
-        assertEquals("192.168.10.20", host.getHostname());
-        assertEquals("INFO", host.getString("LogLevel"));
-        assertTrue(host.getBooleanValue(HostConfig.COMPRESSION, false));
-        assertEquals(COMPRESS_YES, host.getString(KexProposal.PROPOSAL_COMP_CTOS));
+        assertEquals("192.168.10.50", host.getHostname());
+        assertEquals(-1, host.getPort());
 
-        List<String> ciphers = host.getStringList("Ciphers", null);
-        assertEquals(5, ciphers.size());
-        assertEquals("blowfish-cbc", ciphers.get(0));
-        assertEquals("chacha20-poly1305@openssh.com", ciphers.get(1));
-        assertEquals("aes128-ctr", ciphers.get(2));
-        assertEquals("3des-cbc", ciphers.get(3));
-        assertEquals("aes192-cbc", ciphers.get(4));
-
-        final String defCiphers = sshClient.getConfig(KexProposal.PROPOSAL_CIPHER_STOC);
-        assertEquals("chacha20-poly1305@openssh.com,"
-                        + "aes128-ctr,"
-                        + "aes192-ctr,"
-                        + "aes256-ctr,"
-                        + "aes128-gcm@openssh.com,"
-                        + "aes256-gcm@openssh.com",
-                defCiphers,
-                "The default cipher list is not as expected. Update the test-code!");
-
-        ciphers = host.getStringList("Ciphers", defCiphers);
-        // [blowfish-cbc, chacha20-poly1305@openssh.com, aes128-ctr, aes192-ctr,
-        // aes128-gcm@openssh.com, aes256-gcm@openssh.com, 3des-cbc, aes192-cbc]
-        //System.out.println(ciphers);
-
-        assertEquals(8, ciphers.size());
-        assertEquals("blowfish-cbc", ciphers.get(0));
-        assertEquals("chacha20-poly1305@openssh.com", ciphers.get(1));
-        assertEquals("aes128-ctr", ciphers.get(2));
-        assertEquals("aes192-ctr", ciphers.get(3));
-        assertEquals("aes128-gcm@openssh.com", ciphers.get(4));
-        assertEquals("aes256-gcm@openssh.com", ciphers.get(5));
-        assertEquals("3des-cbc", ciphers.get(6));
-        assertEquals("aes192-cbc", ciphers.get(7));
+        final List<String> ciphers = host.getStringList(HostConfig.CIPHERS, null);
+        System.out.println(ciphers);
+        assertEquals(2, ciphers.size());
+        assertEquals("aes256-ctr", ciphers.get(0));
+        assertEquals("rijndael-cbc@lysator.liu.se", ciphers.get(1));
     }
 
     @Test
-    void parseFile05() {
-        final HostConfig host = repo.getHostConfig("martell");
-        System.out.println(host);
-
+    void parseFile10() {
+        final HostConfig host = repo.getHostConfig("tyrell");
         assertNotNull(host);
-        assertEquals("192.168.10.50", host.getHostname());
-        assertEquals("oberyn", host.getUser());
-        assertNotEquals("INFO", host.getString("LogLevel"));
+        System.out.println(host);
+        assertEquals("ERROR", host.getString(HostConfig.LOG_LEVEL));
+
         assertTrue(host.getBooleanValue(HostConfig.COMPRESSION, false));
         assertEquals(COMPRESS_YES, host.getString(KexProposal.PROPOSAL_COMP_CTOS));
+
+        assertEquals("oberyn", host.getUser());
+        assertEquals("192.168.10.20", host.getHostname());
+        assertEquals(-1, host.getPort());
+
+        final List<String> ciphers = host.getStringList(HostConfig.CIPHERS, null);
+        System.out.println(ciphers);
+
+        assertEquals(5, ciphers.size());
+        assertEquals("aes256-ctr", ciphers.get(0));
+        assertEquals("rijndael-cbc@lysator.liu.se", ciphers.get(1));
+        assertEquals("chacha20-poly1305@openssh.com", ciphers.get(2));
+        assertEquals("aes128-ctr", ciphers.get(3));
+        assertEquals("aes256-gcm@openssh.com", ciphers.get(4));
     }
 
     @Test
@@ -145,6 +165,6 @@ class OpenSSHHostConfigRepositoryTest
         final String defValue = sshClient.getConfig(HostConfig.KEX_ALGS);
 
         assertEquals(String.join(",", defValue, "diffie-hellman-group1-sha1"),
-                hostConfig.getString(HostConfig.KEX_ALGS, defValue));
+                     hostConfig.getString(HostConfig.KEX_ALGS, defValue));
     }
 }
