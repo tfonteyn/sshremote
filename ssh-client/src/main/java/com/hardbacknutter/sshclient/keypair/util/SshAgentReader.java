f30d21e6a8ca7fb002b15143bff55dd646e4712d
tfonteyn <user@localhost>
2023-02-05 23:26:29 +0000

    docs

--- ssh-client/src/main/java/com/hardbacknutter/sshclient/keypair/util/SshAgentReader.java
+++ ssh-client/src/main/java/com/hardbacknutter/sshclient/keypair/util/SshAgentReader.java
@@ -22,6 +22,10 @@ import java.security.spec.ECPoint;
 import java.util.Arrays;
 
 // URGENT: this class has never been tested
+
+/**
+ * <a href="https://tools.ietf.org/html/draft-miller-ssh-agent-04">draft-miller-ssh-agent-04</a>
+ */
 class SshAgentReader {
 
     @NonNull
@@ -39,7 +43,7 @@ class SshAgentReader {
         return pubKey == null &&
                 prvKey != null &&
                 prvKey.length > 20 &&
-                // Check the length byte; quick and easy
+                // Check the length byte for matching one of the "key type" string lengths.
                 prvKey[0] == 0 && prvKey[1] == 0 && prvKey[2] == 0
                 // "ssh-rsa", "ssh-dsa"
                 && (prvKey[3] == 7
@@ -52,15 +56,43 @@ class SshAgentReader {
         );
     }
 
+    /**
+     * Parse the payload of a SSH_AGENTC_ADD_IDENTITY message.
+     * The generic format for the key SSH_AGENTC_ADD_IDENTITY message is:
+     * <pre>{@code
+     *        byte                    SSH_AGENTC_ADD_IDENTITY
+     *        string                  key type
+     *        byte[]                  key contents
+     *        string                  key comment
+     * }</pre>
+     *
+     * @param identityBlob the byte[] WITHOUT the message byte. i.e. starting with the "key type"
+     *
+     * @return a KeyPair
+     */
     @NonNull
-    public SshKeyPair parse(@NonNull final byte[] prvKey)
+    public SshKeyPair parse(@NonNull final byte[] identityBlob)
             throws IOException, GeneralSecurityException {
 
-        final Buffer buffer = new Buffer(prvKey);
+        // Format for the key SSH_AGENTC_ADD_IDENTITY.
+        // The "constraints" field is only present for the
+        //   SSH_AGENTC_ADD_ID_CONSTRAINED message.
+        // The below parsing expects the "identityBlob"
+
+        final Buffer buffer = new Buffer(identityBlob);
         final String hostKeyAlgorithm = buffer.getJString();
 
         switch (hostKeyAlgorithm) {
             case HostKeyAlgorithm.SSH_RSA: {
+                //       string                  "ssh-rsa"
+                //       mpint                   n
+                //       mpint                   e
+                //       mpint                   d
+                //       mpint                   iqmp
+                //       mpint                   p
+                //       mpint                   q
+                //       string                  comment
+                //       constraint[]            constraints
                 final BigInteger modulus = buffer.getBigInteger();
                 final BigInteger publicExponent = buffer.getBigInteger();
                 final BigInteger privateExponent = buffer.getBigInteger();
@@ -81,6 +113,14 @@ class SshAgentReader {
                 return keyPair;
             }
             case HostKeyAlgorithm.SSH_DSS: {
+                //       string                  "ssh-dss"
+                //       mpint                   p
+                //       mpint                   q
+                //       mpint                   g
+                //       mpint                   y
+                //       mpint                   x
+                //       string                  comment
+                //       constraint[]            constraints
                 final BigInteger p = buffer.getBigInteger();
                 final BigInteger q = buffer.getBigInteger();
                 final BigInteger g = buffer.getBigInteger();
@@ -99,6 +139,13 @@ class SshAgentReader {
             case HostKeyAlgorithm.SSH_ECDSA_SHA2_NISTP256:
             case HostKeyAlgorithm.SSH_ECDSA_SHA2_NISTP384:
             case HostKeyAlgorithm.SSH_ECDSA_SHA2_NISTP521: {
+                //       string                  key type               HostKeyAlgorithm
+                //       string                  ecdsa_curve_name       nistName
+                //       string                  Q                      public w
+                //       mpint                   d                      private s
+                //       string                  comment
+                //       constraint[]            constraints
+
                 buffer.skipString(/* nistName */);
                 final ECPoint w = ECKeyType.decodePoint(buffer.getString());
                 final BigInteger s = buffer.getBigInteger();
@@ -114,6 +161,13 @@ class SshAgentReader {
             }
             case HostKeyAlgorithm.SSH_ED25519:
             case HostKeyAlgorithm.SSH_ED448: {
+                //       string                  "ssh-ed25519"
+                //       string                  ENC(A)                 public key
+                //       string                  k || ENC(A)            private key "k"
+                //                                                      concatenated with public key
+                //       string                  comment
+                //       constraint[]            constraints
+
                 // the public key
                 final byte[] pub_array = buffer.getString();
                 // OpenSSH stores private key in first half of string and duplicate copy
@@ -121,7 +175,6 @@ class SshAgentReader {
                 final EdKeyType type = EdKeyType.getByHostKeyAlgorithm(hostKeyAlgorithm);
                 final byte[] prv_array = Arrays.copyOf(buffer.getString(), type.keySize);
 
-                // the user comment for the key
                 final String comment = buffer.getJString();
 
                 final SshKeyPair keyPair = new KeyPairEdDSA.Builder(config)
