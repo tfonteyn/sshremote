5f5c9382f04038c4c2bb6a2227e20e16b07a9a68
tfonteyn <user@localhost>
2023-02-06 17:01:21 +0000

    updated tests

--- ssh-client/src/test/java/com/hardbacknutter/sshclient/channels/sftp/PutAndGetTests.java
+++ ssh-client/src/test/java/com/hardbacknutter/sshclient/channels/sftp/PutAndGetTests.java
@@ -11,6 +11,9 @@ import com.hardbacknutter.sshclient.utils.SshException;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.io.BufferedReader;
 import java.io.FileOutputStream;
@@ -20,6 +23,7 @@ import java.io.InputStreamReader;
 import java.io.Reader;
 import java.nio.charset.StandardCharsets;
 import java.security.GeneralSecurityException;
+import java.util.stream.Stream;
 
 class PutAndGetTests
         extends BaseConnectionTest {
@@ -33,43 +37,18 @@ class PutAndGetTests
         super.setup(ZIPPER[ZIP]);
     }
 
-    // This test will fail.
-    // The sun.nio.cs.StreamDecoder seems to be created with the default 8192.
-    // Setting our own buffered reader to less than 8192 (instead of the 50_000)
-    // does not solve the issue, as Java seems to use the default StreamDecoder
-    // constructor. i.e. one of the StreamDecoder forInputStreamReader
-    // which always use the default 8192 buffer.
-    // Is this a Java bug ??
-    //
-    //
-    // newPosition > limit: (12711 > 8192)
-    //java.lang.IllegalArgumentException: newPosition > limit: (12711 > 8192)
-    //	at java.base/java.nio.Buffer.createPositionException(Buffer.java:341)
-    //	at java.base/java.nio.Buffer.position(Buffer.java:316)
-    //	at java.base/java.nio.ByteBuffer.position(ByteBuffer.java:1516)
-    //	at java.base/sun.nio.cs.StreamDecoder.readBytes(StreamDecoder.java:276)
-    //	at java.base/sun.nio.cs.StreamDecoder.implRead(StreamDecoder.java:313)
-    //	at java.base/sun.nio.cs.StreamDecoder.read(StreamDecoder.java:188)
-    //	at java.base/java.io.InputStreamReader.read(InputStreamReader.java:177)
-    //	at java.base/java.io.BufferedReader.fill(BufferedReader.java:162)
-    //	at java.base/java.io.BufferedReader.readLine(BufferedReader.java:329)
-    //	at java.base/java.io.BufferedReader.readLine(BufferedReader.java:396)
-    //	at com.hardbacknutter.sshclient.PutAndGetTests.sftp_get_is_offset(PutAndGetTests.java:59)
-    //	at com.hardbacknutter.sshclient.PutAndGetTests.sftp_get_is(PutAndGetTests.java:35)
-    @Test
-    void sftp_get_is()
-            throws SshException, GeneralSecurityException, IOException {
-        sftp_get_is_offset("long.txt", 0);
+    @NonNull
+    private static Stream<Arguments> getArgs() {
+        return Stream.of(
+                Arguments.of("long.txt", 0),
+                Arguments.of(".bashrc", 10)
+        );
     }
 
-    @Test
-    void sftp_get_is_offset()
-            throws SshException, GeneralSecurityException, IOException {
-        sftp_get_is_offset(".bashrc", 10);
-    }
-
-    private void sftp_get_is_offset(@NonNull final String filename,
-                                    final long offset)
+    @ParameterizedTest
+    @MethodSource("getArgs")
+    void sftp_get_is_offset(@NonNull final String filename,
+                            final long offset)
             throws SshException, GeneralSecurityException, IOException {
 
         session = sshClient.getSession(Constants.USERNAME, Constants.HOST, Constants.PORT);
