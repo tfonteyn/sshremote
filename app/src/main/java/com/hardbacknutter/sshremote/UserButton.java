4b0e728fa7d6d4eba66808336841f7ba2efcd716
tomf <user@localhost>
2024-02-10 18:04:38 +0000

    minor renames

--- app/src/main/java/com/hardbacknutter/sshremote/UserButton.java
+++ app/src/main/java/com/hardbacknutter/sshremote/UserButton.java
@@ -7,6 +7,10 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.preference.PreferenceManager;
 
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.Objects;
+
 import com.hardbacknutter.sshclient.ChannelExec;
 import com.hardbacknutter.sshclient.ChannelSession;
 import com.hardbacknutter.sshclient.Session;
@@ -17,84 +21,80 @@ import com.hardbacknutter.sshremote.db.DB;
 import com.hardbacknutter.sshremote.db.Host;
 import com.hardbacknutter.sshremote.ssh.SshHelper;
 
-import java.io.IOException;
-import java.security.GeneralSecurityException;
-import java.util.Objects;
-
-class UserButton {
+public class UserButton {
 
     @NonNull
-    private final Config mConfig;
+    private final Config config;
     @Nullable
-    private final Host mHost;
+    private final Host host;
     @Nullable
-    private final Command mCommand;
+    private final Command command;
     @Nullable
-    private ChannelSession.ExitStatus mExitStatus;
+    private ChannelSession.ExitStatus exitStatus;
     @Nullable
-    private String mOutput;
+    private String output;
 
     UserButton(@NonNull final DB db,
                final int position) {
 
-        final Config config = db.getConfigDao().findByPosition(position);
-        mConfig = Objects.requireNonNullElseGet(config, () -> new Config(position));
+        config = Objects.requireNonNullElseGet(db.getConfigDao().findByPosition(position),
+                                               () -> new Config(position));
 
-        if (mConfig.hostId != 0) {
-            mHost = db.getHostDao().findById(mConfig.hostId);
+        if (config.hostId != 0) {
+            host = db.getHostDao().findById(config.hostId);
         } else {
-            mHost = new Host();
+            host = new Host();
         }
 
-        if (mConfig.commandId != 0) {
-            mCommand = db.getCommandDao().findById(mConfig.commandId);
+        if (config.commandId != 0) {
+            command = db.getCommandDao().findById(config.commandId);
         } else {
-            mCommand = new Command();
+            command = new Command();
         }
     }
 
     int getPosition() {
-        return mConfig.position;
+        return config.position;
     }
 
     void setPosition(final int position) {
-        mConfig.position = position;
+        config.position = position;
     }
 
     public void update(@NonNull final DB db) {
-        db.getConfigDao().update(mConfig);
+        db.getConfigDao().update(config);
     }
 
     @NonNull
     String getLabel() {
-        return mConfig.label != null ? mConfig.label : "";
+        return config.label != null ? config.label : "";
     }
 
     boolean isPersisted() {
-        return mConfig.id != 0;
+        return config.id != 0;
     }
 
     @Nullable
     String getOutput() {
-        return mOutput;
+        return output;
     }
 
     @Nullable
     ChannelSession.ExitStatus getExitStatus() {
-        return mExitStatus;
+        return exitStatus;
     }
 
     void exec(@NonNull final Context context)
             throws NotConfiguredException, IOException, GeneralSecurityException, SshException {
 
-        if (mHost == null || mCommand == null) {
+        if (host == null || command == null) {
             throw new NotConfiguredException();
         }
 
-        mHost.ping();
+        host.ping();
 
-        mOutput = null;
-        mExitStatus = null;
+        output = null;
+        exitStatus = null;
 
         Session session = null;
         ChannelExec channel = null;
@@ -105,24 +105,24 @@ class UserButton {
 
         // For now we don't let the user configure 'strict' on a per-host basis.
         // so we simply override with the global value
-        mHost.strictHostKeyChecking = strict;
+        host.strictHostKeyChecking = strict;
 
-        final SshHelper ssh = new SshHelper(global, mHost);
+        final SshHelper ssh = new SshHelper(global, host);
         try {
             session = ssh.openSession(context, null);
-            channel = ssh.openChannelExec(session, mCommand);
+            channel = ssh.openChannelExec(session, command);
 
-            if (mCommand.isSudo) {
-                final String password = mCommand.sudoPassword.isEmpty()
-                        ? mHost.userPassword
-                        : mCommand.sudoPassword;
+            if (command.isSudo) {
+                final String password = command.sudoPassword.isEmpty()
+                                        ? host.userPassword
+                                        : command.sudoPassword;
                 ssh.writeln(channel, password);
             }
-            mOutput = ssh.read(channel);
+            output = ssh.read(channel);
 
         } finally {
             if (channel != null) {
-                mExitStatus = channel.getExitStatus();
+                exitStatus = channel.getExitStatus();
             }
             ssh.safeClose(session, channel);
         }
