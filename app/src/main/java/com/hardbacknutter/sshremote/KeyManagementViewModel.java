9ff928677b9f70eab87620ce181092a496e72633
hardbacknutter <user@localhost>
2025-02-18 14:46:30 +0000

    don't crash when user selects a binary file

--- app/src/main/java/com/hardbacknutter/sshremote/KeyManagementViewModel.java
+++ app/src/main/java/com/hardbacknutter/sshremote/KeyManagementViewModel.java
@@ -105,6 +105,9 @@ public class KeyManagementViewModel
                     }
                 }
             }
+        } catch (@NonNull final IllegalArgumentException e) {
+            // user most likely tried reading from a binary file
+            throw new IOException("Invalid file?");
         }
 
         return hostLines;
