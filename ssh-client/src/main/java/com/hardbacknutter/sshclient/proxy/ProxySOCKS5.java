2b99e14badc916caa5708995cfc6eee0344aecda
hardbacknutter <user@localhost>
2026-03-11 18:35:01 +0000

    user/pass is optional

--- ssh-client/src/main/java/com/hardbacknutter/sshclient/proxy/ProxySOCKS5.java
+++ ssh-client/src/main/java/com/hardbacknutter/sshclient/proxy/ProxySOCKS5.java
@@ -1,4 +1,3 @@
-
 /*
  This file depends on following documents,
    - RFC 1928  SOCKS Protocol Version 5
@@ -6,13 +5,14 @@
  */
 package com.hardbacknutter.sshclient.proxy;
 
-import org.jspecify.annotations.NonNull;
-
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
+import java.util.Locale;
 
 import com.hardbacknutter.sshclient.SocketFactory;
+
+import org.jspecify.annotations.NonNull;
 
 /**
  * A {@link Proxy} implementation using a SOCKS V5 proxy.
@@ -21,9 +21,9 @@ import com.hardbacknutter.sshclient.SocketFactory;
  * to connect to the desired server.
  *
  * @see <a href="https://datatracker.ietf.org/doc/html/rfc1928">
- * RFC 1928 SOCKS Protocol Version 5.</a>
+ *         RFC 1928 SOCKS Protocol Version 5.</a>
  * @see <a href="https://datatracker.ietf.org/doc/html/rfc1929">
- * RFC 1929 Username/Password Authentication for SOCKS V5</a>
+ *         RFC 1929 Username/Password Authentication for SOCKS V5</a>
  */
 public class ProxySOCKS5
         extends ProxyBase {
@@ -69,79 +69,90 @@ public class ProxySOCKS5
 
             final byte[] buf = new byte[1024];
             int index = 0;
-
-/*
+            int nMethods = 1;
+            if (user != null && passwd != null) {
+                nMethods++;
+            }
+            /*
                    +----+----------+----------+
                    |VER | NMETHODS | METHODS  |
                    +----+----------+----------+
                    | 1  |    1     | 1 to 255 |
                    +----+----------+----------+
 
-   The VER field is set to X'05' for this version of the protocol.  The
-   NMETHODS field contains the number of method identifier octets that
-   appear in the METHODS field.
+            The VER field is set to X'05' for this version of the protocol.  The
+            NMETHODS field contains the number of method identifier octets that
+            appear in the METHODS field.
 
-   The values currently defined for METHOD are:
+            The values currently defined for METHOD are:
 
-          o  X'00' NO AUTHENTICATION REQUIRED
-          o  X'01' GSSAPI
-          o  X'02' USERNAME/PASSWORD
-          o  X'03' to X'7F' IANA ASSIGNED
-          o  X'80' to X'FE' RESERVED FOR PRIVATE METHODS
-          o  X'FF' NO ACCEPTABLE METHODS
-*/
-
+                  o  X'00' NO AUTHENTICATION REQUIRED
+                  o  X'01' GSSAPI
+                  o  X'02' USERNAME/PASSWORD
+                  o  X'03' to X'7F' IANA ASSIGNED
+                  o  X'80' to X'FE' RESERVED FOR PRIVATE METHODS
+                  o  X'FF' NO ACCEPTABLE METHODS
+            */
+            // VER
             buf[index++] = 5;
-
-            buf[index++] = 2;
-            buf[index++] = 0;           // NO AUTHENTICATION REQUIRED
-            buf[index++] = 2;           // USERNAME/PASSWORD
+            // NMETHODS
+            buf[index++] = (byte) nMethods;
+            // METHOD: NO AUTHENTICATION REQUIRED
+            buf[index++] = 0;
+            if (user != null && passwd != null) {
+                // METHOD: USERNAME/PASSWORD
+                buf[index++] = 2;
+            }
 
             //noinspection DataFlowIssue
             out.write(buf, 0, index);
 
-/*
-    The server selects from one of the methods given in METHODS, and
-    sends a METHOD selection message:
+            /*
+                The server selects from one of the methods given in METHODS, and
+                sends a METHOD selection message:
 
                          +----+--------+
                          |VER | METHOD |
                          +----+--------+
                          | 1  |   1    |
                          +----+--------+
-*/
+            */
             //noinspection DataFlowIssue
             fill(in, buf, 2);
 
             boolean check = false;
-            switch ((buf[1]) & 0xff) {
-                case 0:                // NO AUTHENTICATION REQUIRED
+            final int authMethod = buf[1] & 0xff;
+            String failureMsg = null;
+            switch (authMethod) {
+                case 0: { // NO AUTHENTICATION REQUIRED
                     check = true;
                     break;
-                case 2:                // USERNAME/PASSWORD
+                }
+                case 2: { // USERNAME/PASSWORD
                     if (user == null || passwd == null) {
+                        failureMsg = "SOCKS5 username/password authentication requested"
+                                     + " by the server but no username/password configured";
                         break;
                     }
+                    /*
+                       Once the SOCKS V5 server has started, and the client has selected the
+                       Username/Password Authentication protocol, the Username/Password
+                       sub-negotiation begins.  This begins with the client producing a
+                       Username/Password request:
 
-/*
-   Once the SOCKS V5 server has started, and the client has selected the
-   Username/Password Authentication protocol, the Username/Password
-   sub-negotiation begins.  This begins with the client producing a
-   Username/Password request:
+                               +----+------+----------+------+----------+
+                               |VER | ULEN |  UNAME   | PLEN |  PASSWD  |
+                               +----+------+----------+------+----------+
+                               | 1  |  1   | 1 to 255 |  1   | 1 to 255 |
+                               +----+------+----------+------+----------+
 
-           +----+------+----------+------+----------+
-           |VER | ULEN |  UNAME   | PLEN |  PASSWD  |
-           +----+------+----------+------+----------+
-           | 1  |  1   | 1 to 255 |  1   | 1 to 255 |
-           +----+------+----------+------+----------+
-
-   The VER field contains the current version of the subnegotiation,
-   which is X'01'. The ULEN field contains the length of the UNAME field
-   that follows. The UNAME field contains the username as known to the
-   source operating system. The PLEN field contains the length of the
-   PASSWD field that follows. The PASSWD field contains the password
-   association with the given UNAME.
-*/
+                       The VER field contains the current version of the subnegotiation,
+                       which is X'01'. The ULEN field contains the length of the UNAME field
+                       that follows. The UNAME field contains the username as known to the
+                       source operating system. The PLEN field contains the length of the
+                       PASSWD field that follows. The PASSWD field contains the password
+                       association with the given UNAME.
+                    */
                     index = 0;
                     buf[index++] = 1;
 
@@ -157,57 +168,70 @@ public class ProxySOCKS5
 
                     out.write(buf, 0, index);
 
-/*
-   The server verifies the supplied UNAME and PASSWD, and sends the
-   following response:
+                    /*
+                       The server verifies the supplied UNAME and PASSWD, and sends the
+                       following response:
 
-                        +----+--------+
-                        |VER | STATUS |
-                        +----+--------+
-                        | 1  |   1    |
-                        +----+--------+
+                                            +----+--------+
+                                            |VER | STATUS |
+                                            +----+--------+
+                                            | 1  |   1    |
+                                            +----+--------+
 
-   A STATUS field of X'00' indicates success. If the server returns a
-   `failure' (STATUS value other than X'00') status, it MUST close the
-   connection.
-*/
+                       A STATUS field of X'00' indicates success. If the server returns a
+                       `failure' (STATUS value other than X'00') status, it MUST close the
+                       connection.
+                    */
                     fill(in, buf, 2);
-                    if (buf[1] == 0) {
+                    final int status = buf[1] & 0xff;
+                    if (status == 0) {
                         check = true;
+                    } else {
+                        failureMsg = String.format(
+                                Locale.ROOT,
+                                "SOCKS5 username/password authentication failed:"
+                                + " server status = %02X", status);
                     }
                     break;
-                default:
+                }
+                default: {
+                    failureMsg = String.format(
+                            Locale.ROOT,
+                            "Unsupported SOCKS5 authentication method requested"
+                            + " by the server: %02X", authMethod);
+                    break;
+                }
             }
 
             if (!check) {
-                throw new SshProxyException("fail in SOCKS5 proxy");
+                throw new SshProxyException(failureMsg);
             }
 
-/*
-      The SOCKS request is formed as follows:
+            /*
+                  The SOCKS request is formed as follows:
 
-        +----+-----+-------+------+----------+----------+
-        |VER | CMD |  RSV  | ATYP | DST.ADDR | DST.PORT |
-        +----+-----+-------+------+----------+----------+
-        | 1  |  1  | X'00' |  1   | Variable |    2     |
-        +----+-----+-------+------+----------+----------+
+                    +----+-----+-------+------+----------+----------+
+                    |VER | CMD |  RSV  | ATYP | DST.ADDR | DST.PORT |
+                    +----+-----+-------+------+----------+----------+
+                    | 1  |  1  | X'00' |  1   | Variable |    2     |
+                    +----+-----+-------+------+----------+----------+
 
-      Where:
+                  Where:
 
-      o  VER    protocol version: X'05'
-      o  CMD
-         o  CONNECT X'01'
-         o  BIND X'02'
-         o  UDP ASSOCIATE X'03'
-      o  RSV    RESERVED
-         o  ATYP   address type of following address
-         o  IP V4 address: X'01'
-         o  DOMAINNAME: X'03'
-         o  IP V6 address: X'04'
-      o  DST.ADDR       desired destination address
-      o  DST.PORT desired destination port in network octet
-         order
-*/
+                  o  VER    protocol version: X'05'
+                  o  CMD
+                     o  CONNECT X'01'
+                     o  BIND X'02'
+                     o  UDP ASSOCIATE X'03'
+                  o  RSV    RESERVED
+                     o  ATYP   address type of following address
+                     o  IP V4 address: X'01'
+                     o  DOMAINNAME: X'03'
+                     o  IP V6 address: X'04'
+                  o  DST.ADDR       desired destination address
+                  o  DST.PORT desired destination port in network octet
+                     order
+            */
 
             index = 0;
             buf[index++] = 5;
@@ -225,40 +249,40 @@ public class ProxySOCKS5
 
             out.write(buf, 0, index);
 
-/*
-   The SOCKS request information is sent by the client as soon as it has
-   established a connection to the SOCKS server, and completed the
-   authentication negotiations.  The server evaluates the request, and
-   returns a reply formed as follows:
+            /*
+               The SOCKS request information is sent by the client as soon as it has
+               established a connection to the SOCKS server, and completed the
+               authentication negotiations.  The server evaluates the request, and
+               returns a reply formed as follows:
 
-        +----+-----+-------+------+----------+----------+
-        |VER | REP |  RSV  | ATYP | BND.ADDR | BND.PORT |
-        +----+-----+-------+------+----------+----------+
-        | 1  |  1  | X'00' |  1   | Variable |    2     |
-        +----+-----+-------+------+----------+----------+
+                    +----+-----+-------+------+----------+----------+
+                    |VER | REP |  RSV  | ATYP | BND.ADDR | BND.PORT |
+                    +----+-----+-------+------+----------+----------+
+                    | 1  |  1  | X'00' |  1   | Variable |    2     |
+                    +----+-----+-------+------+----------+----------+
 
-   Where:
+               Where:
 
-   o  VER    protocol version: X'05'
-   o  REP    Reply field:
-      o  X'00' succeeded
-      o  X'01' general SOCKS server failure
-      o  X'02' connection not allowed by ruleset
-      o  X'03' Network unreachable
-      o  X'04' Host unreachable
-      o  X'05' Connection refused
-      o  X'06' TTL expired
-      o  X'07' Command not supported
-      o  X'08' Address type not supported
-      o  X'09' to X'FF' unassigned
-      o  RSV    RESERVED
-      o  ATYP   address type of following address
-      o  IP V4 address: X'01'
-      o  DOMAINNAME: X'03'
-      o  IP V6 address: X'04'
-    o  BND.ADDR       server bound address
-    o  BND.PORT       server bound port in network octet order
-*/
+               o  VER    protocol version: X'05'
+               o  REP    Reply field:
+                  o  X'00' succeeded
+                  o  X'01' general SOCKS server failure
+                  o  X'02' connection not allowed by ruleset
+                  o  X'03' Network unreachable
+                  o  X'04' Host unreachable
+                  o  X'05' Connection refused
+                  o  X'06' TTL expired
+                  o  X'07' Command not supported
+                  o  X'08' Address type not supported
+                  o  X'09' to X'FF' unassigned
+                  o  RSV    RESERVED
+                  o  ATYP   address type of following address
+                  o  IP V4 address: X'01'
+                  o  DOMAINNAME: X'03'
+                  o  IP V6 address: X'04'
+                o  BND.ADDR       server bound address
+                o  BND.PORT       server bound port in network octet order
+            */
 
             fill(in, buf, 4);
 
@@ -281,6 +305,7 @@ public class ProxySOCKS5
             }
 
         } catch (final IOException | SshProxyException e) {
+            // close all, then simply rethrow
             close();
             throw e;
         }
