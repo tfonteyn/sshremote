950228510c2d21cb67c77c2a14e70aace85a7038
hardbacknutter <user@localhost>
2026-03-13 15:12:37 +0000

    bump version for next release

--- ssh-client/src/main/java/com/hardbacknutter/sshclient/SshClientFactory.java
+++ ssh-client/src/main/java/com/hardbacknutter/sshclient/SshClientFactory.java
@@ -18,7 +18,7 @@ import com.hardbacknutter.sshclient.utils.SshClientConfigImpl;
  */
 public final class SshClientFactory {
 
-    private static final String VERSION = "1.5.0";
+    private static final String VERSION = "1.5.1";
     private static final String META_INF_MANIFEST_MF = "META-INF/MANIFEST.MF";
     private static final String MANIFEST_VERSION = "Manifest-Version";
 
